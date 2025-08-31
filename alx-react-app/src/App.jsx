d773b9808f37b3a7f25ba7c082d2d9fe3aa162bb
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-08-31 21:12:23 +0100

    components added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import MainsContent from './components/Maincontent'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -30,6 +33,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage /> 
+      <Header />
+      <MainsContent />
+      <Footer />
     </>
   )
 }
---
+++ alx-react-app/src/components/Maincontent.jsx
@@ -0,0 +1,9 @@
+function MainsContent() {
+    return (
+        <main>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainsContent;
