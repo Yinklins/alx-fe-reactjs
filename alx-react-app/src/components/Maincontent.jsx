3cf77348dda7296928f48ea421be23576383ae0d
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-08-31 21:18:59 +0100

    MainContent.jsx correct

--- alx-react-app/src/components/Maincontent.jsx
+++ alx-react-app/src/components/Maincontent.jsx
@@ -1,4 +1,4 @@
-function MainsContent() {
+function MainContent() {
     return (
         <main>
             <p>I love to visit New York, Paris, and Tokyo.</p>
@@ -6,4 +6,4 @@ function MainsContent() {
     );
 }
 
-export default MainsContent;
+export default MainContent;
