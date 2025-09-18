d8a0bff0fc3833de4e426de1f9b3127a472c3d8c
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-09-18 04:15:14 +0100

    browser updated

--- recipe-sharing-app/src/components/browser.jsx
+++ recipe-sharing-app/src/components/browser.jsx
@@ -3,16 +3,21 @@ import RecipeList from "./RecipeList";
 import RecipeDetails from "./RecipeDetails";
 import EditRecipeForm from "./EditRecipeForm";
 
-function AppRouter() {
+function App() {
   return (
     <Router>
       <Routes>
+        {/* Home / All Recipes */}
         <Route path="/" element={<RecipeList />} />
+
+        {/* Recipe Details Page */}
         <Route path="/recipes/:id" element={<RecipeDetails />} />
+
+        {/* Edit Recipe Page */}
         <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
       </Routes>
     </Router>
   );
 }
 
-export default AppRouter;
+export default App;
