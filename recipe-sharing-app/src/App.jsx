701eab7370658e75132bea1511897c962992b412
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-09-18 04:38:21 +0100

    file updated

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 import AddRecipeForm from './components/AddRecipeForm'
-import AppRouter from './components/browser'
 import RecipeList from './components/RecipeList'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeDetails from "./components/RecipeDetails";
+import EditRecipeForm from "./components/EditRecipeForm";
+
+
 
 
 function App() {
@@ -12,7 +16,20 @@ function App() {
       <h1>This is all about Adding new recipes </h1>
       <RecipeList />
       <AddRecipeForm />
-      <AppRouter />      
+
+      <Router>
+      <Routes>
+        {/* Home / All Recipes */}
+        <Route path="/" element={<RecipeList />} />
+
+        {/* Recipe Details Page */}
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+
+        {/* Edit Recipe Page */}
+        <Route path="/recipes/:id/edit" element={<EditRecipeForm />} />
+      </Routes>
+    </Router>
+           
     </>
   )
 }
