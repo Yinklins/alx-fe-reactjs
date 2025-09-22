a0c7b9de0adb26c7fae129326a5c5cfa631f7aa7
Olayinka Adeyeye <93340269+Yinklins@users.noreply.github.com>
2025-09-22 11:46:18 +0100

    file correcton made

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -23,3 +23,17 @@ export const fetchUserData = async (username) => {
   const response = await axios.get(`https://api.github.com/users/${username}`);
   return response.data;
 };
+
+
+const BASE_URL = "https://api.github.com";
+
+export const fetchAdvancedUsers = async (username, location, minRepos) => {
+  let query = "";
+
+  if (username) query += `${username} in:login `;
+  if (location) query += `location:${location} `;
+  if (minRepos) query += `repos:>=${minRepos}`;
+
+  const response = await axios.get(`${BASE_URL}/search/users?q=${query.trim()}`);
+  return response.data;
+};
---
+++ github-user-search/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",  // ✅ add this
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
